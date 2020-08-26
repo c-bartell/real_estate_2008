5a9f49cfac8ecf6cc3437d9994aef6cced5f631c
Curtis Bartell <cbartell980@gmail.com>
2020-08-26 09:41:34 -0600

    Add setup

---
+++ test/house_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/room"
+require "./lib/house"
+
+class HouseTest < Minitest::Test
+  def setup
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+  end
+end
