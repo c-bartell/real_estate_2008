eb92a12041d5a552cddbbf6f208f69ec2204469a
Curtis Bartell <cbartell980@gmail.com>
2020-08-26 09:18:03 -0600

    Add test_is_not_painted_by_default

--- test/room_test.rb
+++ test/room_test.rb
@@ -21,4 +21,9 @@ class RoomTest < Minitest::Test
     assert_equal 130, room1.area
     assert_equal 180, room2.area
   end
+
+  def test_is_not_painted_by_default
+    room1 = Room.new(:bedroom, 10, '13')
+    assert_equal false, room1.is_painted?
+  end
 end
