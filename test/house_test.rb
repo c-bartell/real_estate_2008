5badf575329b42283f5a5856442fd986889e9714
Curtis Bartell <cbartell980@gmail.com>
2020-08-26 09:43:01 -0600

    Add test_it_exists

--- test/house_test.rb
+++ test/house_test.rb
@@ -5,8 +5,12 @@ require "./lib/house"
 
 class HouseTest < Minitest::Test
   def setup
-    house = House.new("$400000", "123 sugar lane")
-    room_1 = Room.new(:bedroom, 10, '13')
-    room_2 = Room.new(:bedroom, 11, '15')
+    @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
+  end
+
+  def test_it_exists
+    assert_instance_of House, @house
   end
 end
